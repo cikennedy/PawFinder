6cc0694fd01f32bdc60bb9a1c082e7f6ece8b288
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-20 15:51:06 -0500

    directory cleanup

---
+++ pawfinderbackend/schemas/post.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'post',
+    type: 'document',
+    title: 'Missing Pet Post',
+    fields: [
+        {
+            name: 'postedAt',
+            type: 'geopoint',
+            title: 'Location'
+        }
+    ]
+}
